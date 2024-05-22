b4909f72e1b07d62504ee193a8452eff0bceef71
the-man2004 <heinrichhb111@gmail.com>
2024-05-22 15:57:21 -0800

    Added a NewProject component for adding new projects.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import Project from "./components/Project";
 import Sidebar from "./components/Sidebar";
 
-import noProjects from "./assets/no-projects.png";
 import NotSelected from "./components/NotSelected";
+import AddProject from "./components/AddProject";
 
 function App() {
+  const [addNewProject, setAddNewProject] = useState(true);
   const [selectedProject, setSelectedProject] = useState(null);
 
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 min-h-screen">
       <Sidebar />
-      {!selectedProject ? <NotSelected /> : <Project />}
+      {selectedProject === null && !addNewProject && <NotSelected />}
+      {selectedProject !== null && !addNewProject && <Project />}
+      {addNewProject && <AddProject />}
     </div>
   );
 }
---
+++ src/components/AddProject.jsx
@@ -0,0 +1,32 @@
+export default function AddProject() {
+  return (
+    <div className="col-span-2 max-w-[750px] mt-8 p-8">
+      <div className="mt-16 flex justify-end">
+        <button className=" py-2 px-6 rounded-lg">Cancel</button>
+        <button className="py-2 px-6 bg-stone-900 text-white rounded-lg">
+          Save
+        </button>
+      </div>
+      <form className="mt-8 flex flex-col font-bold">
+        <label htmlFor="title">TITLE</label>
+        <input
+          type="text"
+          id="title"
+          className="mb-4 font-semibold bg-stone-200 outline-none focus:border-b-2 border-stone-900"
+        />
+        <label htmlFor="description">DESCRIPTION</label>
+        <textarea
+          name="description"
+          id="description"
+          className="mb-4 h-20 font-semibold bg-stone-200 outline-none focus:border-b-2 border-stone-900"
+        ></textarea>
+        <label htmlFor="">DUE DATE</label>
+        <input
+          type="date"
+          id="title"
+          className="mb-4 font-semibold bg-stone-200 outline-none focus:border-b-2 border-stone-900"
+        />
+      </form>
+    </div>
+  );
+}

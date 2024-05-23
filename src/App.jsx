b15b300f2f04c3893bbb029e54e1540b16fcd68e
the-man2004 <heinrichhb111@gmail.com>
2024-05-22 16:45:04 -0800

    Now outputting the basic project info in Project.jsx (not styled yet).

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ import NotSelected from "./components/NotSelected";
 import AddProject from "./components/AddProject";
 
 function App() {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([
+    {
+      title: "Learning React from Maximilian",
+      description: `
+      Learning React from Maximilian
+
+      Start with the basics, end with the advanced stuff.
+      `,
+      dueDate: "05/23/2024",
+    },
+  ]);
   const [addNewProject, setAddNewProject] = useState(false);
   const [selectedProject, setSelectedProject] = useState(null);
 
@@ -19,16 +29,30 @@ function App() {
   }
 
   function handleNewProject(newProject) {
-    setProjects((prevProjects) => [...prevProjects, newProject]);
+    setProjects((prevProjects) => [newProject, ...prevProjects]);
+    setAddNewProject(false);
   }
+
+  function handleSetSelectedProject(projectPosition) {
+    setSelectedProject(projectPosition);
+    console.log(projectPosition);
+  }
+
+  const currentProject = projects[selectedProject];
 
   return (
     <div className="grid grid-cols-3 min-h-screen">
-      <Sidebar onAddProjectClick={handleAddProjectClick} />
+      <Sidebar
+        onAddProjectClick={handleAddProjectClick}
+        projects={projects}
+        onProjectSelect={handleSetSelectedProject}
+      />
       {selectedProject === null && !addNewProject && (
         <NotSelected onAddProjectClick={handleAddProjectClick} />
       )}
-      {selectedProject !== null && !addNewProject && <Project />}
+      {selectedProject !== null && !addNewProject && (
+        <Project project={currentProject} />
+      )}
       {addNewProject && (
         <AddProject onClose={handleAddProjectClick} onSave={handleNewProject} />
       )}
